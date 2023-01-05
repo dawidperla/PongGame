778764995dbea1aa7f24c4f47f282044f7fe0754
dawidperla <108357006+dawidperla@users.noreply.github.com>
2023-01-05 06:32:33 +0100

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,7 +1,9 @@
 from turtle import Turtle
 
+
 class Ball(Turtle):
 
+    
     def __init__(self):
         super().__init__()
         self.shape("circle")
@@ -14,7 +16,6 @@ class Ball(Turtle):
 
 
     def move(self):
-
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x,new_y)
@@ -22,10 +23,12 @@ class Ball(Turtle):
 
     def bounce_y(self):
         self.y_move *= -1
+        
 
     def bounce_x(self):
         self.x_move *= -1
         self.move_speed *= 0.9
+        
 
     def game_over(self):
         self.goto(0, 0)
