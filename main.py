2514d612cdccee13da7d8b4a4032db430d27c7f1
dawidperla <108357006+dawidperla@users.noreply.github.com>
2023-01-05 06:33:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game")
 screen.tracer(0)
-
 r_pad = Paddle((350, 0))
 l_pad = Paddle((-350, 0))
+
+
 ball = Ball()
 board = Scoreboard()
 
@@ -20,6 +21,7 @@ screen.onkey(r_pad.go_up,"Up")
 screen.onkey(r_pad.go_down,"Down")
 screen.onkey(l_pad.go_up,"w")
 screen.onkey(l_pad.go_down,"s")
+
 
 game_is_on = True
 while game_is_on:
@@ -32,6 +34,7 @@ while game_is_on:
     if ball.ycor() > 280 or ball.ycor() < -280:
         #need bounce
         ball.bounce_y()
+        
 
     #detect collision with the paddle r
     if ball.distance(r_pad) < 50 and ball.xcor() > 320 or ball.distance(l_pad) < 50 and ball.xcor() < -320:
@@ -41,13 +44,11 @@ while game_is_on:
     if ball.xcor() > 380:
         ball.reset_position()
         board.l_point()
+        
 
     if ball.xcor() < -380:
         ball.reset_position()
         board.r_point()
-
-
-
 
 
 screen.exitonclick()
